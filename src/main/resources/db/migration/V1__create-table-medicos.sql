03d7915c62304f24c2e73d3e254e622a31547331
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-03-06 18:32:01 -0300

    configurando novos users em memoria interna

--- src/main/resources/db/migration/V1__create-table-medicos.sql
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -1,10 +1,8 @@
-create table medicos(
-    id bigint not null auto_increment,
-    nome varchar(100) not null,
-    email varchar(100) not null unique,
-    telefone varchar(20) not null,
-    crm varchar(6) not null unique,
-    especialidade varchar(100) not null,
-
-    primary key(id)
+CREATE TABLE medicos (
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefone VARCHAR(20) NOT NULL,
+    crm VARCHAR(6) NOT NULL UNIQUE,
+    especialidade VARCHAR(100) NOT NULL
 );

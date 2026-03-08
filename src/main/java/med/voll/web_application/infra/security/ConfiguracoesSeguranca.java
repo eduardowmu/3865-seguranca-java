007427386858c05173623c268c50dae1953d8f96
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-03-08 12:00:32 -0300

    cookies

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,11 +2,13 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -26,5 +28,20 @@ public class ConfiguracoesSeguranca {
                 .password("{noop}edu123").build();
 
         return new InMemoryUserDetailsManager(usuario1, usuario2, usuario3);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(req -> {
+                            req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                            req.anyRequest().authenticated();
+                        }
+                ).formLogin(f -> f.loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout")
+                        .permitAll())
+                .build();
     }
 }

3509190df9ffe31ea654cfa8ec94c69a60b90d2e
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-03-09 08:50:20 -0300

    csrf

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,6 +2,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -42,6 +43,10 @@ public class ConfiguracoesSeguranca {
                         .permitAll())
                 .logout(logout -> logout.logoutSuccessUrl("/login?logout")
                         .permitAll())
+                .rememberMe(r -> r.key("rememberMe")
+                        .alwaysRemember(true)
+                //        .tokenValiditySeconds()
+                ).csrf(Customizer.withDefaults())
                 .build();
     }
 }
